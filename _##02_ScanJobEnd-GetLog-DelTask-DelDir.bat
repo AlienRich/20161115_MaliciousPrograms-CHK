07aa0a22c228a3ab736a3262a5bcbf5d6f4abbfa
AlienRich <sun0731@gmail.com>
2017-01-19 11:19:10 +0800

    20170118-修正無法Get回檔案問題

--- _##02_ScanJobEnd-GetLog-DelTask-DelDir.bat
+++ _##02_ScanJobEnd-GetLog-DelTask-DelDir.bat
@@ -2,7 +2,10 @@
 
 set FilePath=%CD%
 set LogFilePath=%CD%\_@_Get-ScanLog
-Set 7zLogType=201?????_*_*.*.*.*_Data.7z
+mkdir %LogFilePath%
+Set LogType7z=201?????_*_*.*.*.*_Data.7z
+set NoLog=%CD%\--RunJobButNoLog.txt
+set YesLog=%CD%\--RunJobLogOK.txt
 
 
 for /f %%r in ('whoami') do (set RunID=%%r)
@@ -19,12 +22,15 @@ set RoboLog=.\--Robo.Log
 ::::	del %RoboLog%
 ::::	檔案名稱範例	20170118_xcarddb_169.254.95.120_Data.7z
 
-COLOR 0B
+
+	COLOR 0B
 TITLE "清除殘留TASK、Get執行結果 ::: %RunID%"
 for /F %%i IN (%CopyOKIP%) do (
 	echo TaskKill ::: %%i
 	if exist \\%%i\c$\*.* start robocopy C:\temp \\%%i\c$\temp _#_TaskKill-_FTool.bat /r:3 /w:0
 	ping 127.0.0.1 -w 800 -n 5
 	if exist \\%%i\c$\temp\_#_TaskKill-_FTool.bat start .\psexec \\%%i C:\temp\_#_TaskKill-_FTool.bat
-	if exist \\%%i\c$\%7zLogType% start robocopy \\%%i\c$ %LogFilePath% %7zLogType% /mov /r:3 /w:0
+	DIR /B \\%%i\c$\%LogType7z% >> %YesLog%
+	DIR /B \\%%i\c$\%LogType7z%
+	if not exist \\%%i\c$\%LogType7z% (Echo %%i_未產生LOG>>%NoLog%) ELSE (robocopy \\%%i\c$ %LogFilePath% %LogType7z% /mov /r:3 /w:0)
 	)
